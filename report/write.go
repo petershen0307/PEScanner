6c2aaf8e80e186380b5eee4a2f516923c1c28f49
PeterShen <petershen0307@hotmail.com>
2021-08-16 23:35:09 +0800

    fixed csv format

--- report/write.go
+++ report/write.go
@@ -26,7 +26,7 @@ func Write(outputDir string, mode models.ScanMode, reports []models.Report) {
 func WriteProfiling(outputDir string, mode models.ScanMode, start, end time.Time, scanFiles, peFiles int) {
 	filePath := filepath.Join(outputDir, fmt.Sprintf("%v-%v.csv", mode.String(), time.Now().UTC().Unix()))
 	header := "mode, start(MicroSec), end(MicroSec), execution(MicroSec), scanFiles, peFiles"
-	values := fmt.Sprintf("%v,%v,%v,%v.%v,%v", mode.String(), start.UnixNano()/1000, end.UnixNano()/1000, end.Sub(start).Microseconds(), scanFiles, peFiles)
+	values := fmt.Sprintf("%v,%v,%v,%v,%v,%v", mode.String(), start.UnixNano()/1000, end.UnixNano()/1000, end.Sub(start).Microseconds(), scanFiles, peFiles)
 	outputStr := strings.Join([]string{header, values}, "\n")
 	os.WriteFile(filePath, []byte(outputStr), 0o755)
 }
